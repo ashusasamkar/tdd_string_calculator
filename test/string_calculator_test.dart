dff32416a53a3cf08b85a08ed81e5ffe6364c5b6
Ashvinee Sasamkar <ashusasamkar@gmail.com>
2026-01-15 19:16:09 +0530

    Feature 1 – failing test test: add test for empty string returning 0

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_string_calculator/data/calculators/string_calculator.dart';
+
+void main() {
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+  test("Empty string should return 0", () {
+    expect(calculator.add(''), 0);
+  });
+}
