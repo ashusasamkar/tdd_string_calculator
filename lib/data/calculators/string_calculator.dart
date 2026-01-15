2e685e5f6cadb5e9cd66eb1ddaaba4d8a5220e35
Ashvinee Sasamkar <ashusasamkar@gmail.com>
2026-01-15 19:37:24 +0530

    Feature 5 – implementation feat: supports newline and comma delimiters

--- lib/data/calculators/string_calculator.dart
+++ lib/data/calculators/string_calculator.dart
@@ -2,6 +2,8 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    return numbers.split(",").map(int.parse).fold(0, (sum, n) => sum + n);
+    final sanitized = numbers.replaceAll('\n', ',');
+
+    return sanitized.split(',').map(int.parse).fold(0, (sum, n) => sum + n);
   }
 }
