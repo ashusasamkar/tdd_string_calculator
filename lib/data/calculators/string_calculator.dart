bcb51f6ede1c10346e5339fdbb53c548aee3c8f9
Ashvinee Sasamkar <ashusasamkar@gmail.com>
2026-01-15 19:31:46 +0530

    Feature 4 – implementation feat: support any amount of numbers separated by commas

--- lib/data/calculators/string_calculator.dart
+++ lib/data/calculators/string_calculator.dart
@@ -2,9 +2,6 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(',');
-    if (parts.length == 1) return int.parse(parts[0]);
-
-    return int.parse(parts[0]) + int.parse(parts[1]);
+    return numbers.split(",").map(int.parse).fold(0, (sum, n) => sum + n);
   }
 }
