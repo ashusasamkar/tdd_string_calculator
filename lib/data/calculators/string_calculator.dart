06c58d16d3fb704fcc732a36b9a6a3b721653383
Ashvinee Sasamkar <ashusasamkar@gmail.com>
2026-01-15 19:44:14 +0530

    Feature 6 – implementation feat:  modified code for custom delimiter is supported

--- lib/data/calculators/string_calculator.dart
+++ lib/data/calculators/string_calculator.dart
@@ -2,8 +2,20 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final sanitized = numbers.replaceAll('\n', ',');
+    String delimiter = ',';
+    String numbersPart = numbers;
 
-    return sanitized.split(',').map(int.parse).fold(0, (sum, n) => sum + n);
+    if (numbers.startsWith('//')) {
+      final lines = numbers.split('\n');
+      delimiter = lines[0].substring(2);
+      numbersPart = lines[1];
+    }
+
+    numbersPart = numbersPart.replaceAll('\n', delimiter);
+
+    return numbersPart
+        .split(delimiter)
+        .map(int.parse)
+        .fold(0, (sum, n) => sum + n);
   }
 }
